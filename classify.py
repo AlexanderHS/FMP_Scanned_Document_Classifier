0a093dce1ea4a7ab7c575ebc389915fe488dfc48
Alex Hamilton-Smith <alexhamiltonsmith@gmail.com>
2021-02-25 17:32:33 +1100

    tweak to not overwrite previous

--- classify.py
+++ classify.py
@@ -64,7 +64,7 @@ def move_to_unclassified(filepath):
     except Exception:
         pass
     os.remove(filepath)
-
+    
 def move_to_classified(filepath, batch, aw_no):
     dest_path = '/mnt/scans/Batch Records/'
     #dest_path = 'Batch_Records/'
@@ -74,7 +74,10 @@ def move_to_classified(filepath, batch, aw_no):
     if not os.path.exists(dest_path):
         os.makedirs(dest_path)
     destination_full = dest_path + batch + ' ' + aw_no + '.pdf'
-    #filepath = ntpath.basename(filepath)
+    index = 0;
+    while os.path.exists(destination_full):
+        index += 1
+        destination_full = dest_path + batch + ' ' + aw_no + index.zfill(3) + '.pdf'
     try:
         shutil.copy(filepath, destination_full)
     except PermissionError:
