03ad82272801830b4bc875dcac6ddac16e91acbf
Alex Hamilton-Smith <Alex.Hamilton-Smith@fairmontmedical.com.au>
2021-03-29 11:24:09 +1100

    added /c to run

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-cmd /k "cd /d C:\Users\Alex.Hamilton-Smith\vs_root\FMP_Scanned_Document_Classifier\ENV\Scripts & activate & cd /d    C:\Users\Alex.Hamilton-Smith\vs_root\FMP_Scanned_Document_Classifier & python classify.py"
+cmd /c /k "cd /d C:\Users\Alex.Hamilton-Smith\vs_root\FMP_Scanned_Document_Classifier\ENV\Scripts & activate & cd /d    C:\Users\Alex.Hamilton-Smith\vs_root\FMP_Scanned_Document_Classifier & python classify.py"
 exit
